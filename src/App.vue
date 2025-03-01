7494ef3351112f6de8c3b8c3212aa20cfb1509da
Banopartt <turgaliev.almat11@gmail.com>
2025-03-01 10:42:39 +0600

    added axios and request for json placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,40 @@
 <template>
   <div>
-    <TodoEdit v-model:is-open-modal="isOpenModal"/>
-    <TodoForm
-        :error-message="errorMessage"
-        v-model:title="title"
-        v-model:description="description"
-        @create-todo="createTodo"
-    />
-    <div class="wrapper" v-if="todoCards.length">
-      <TodoCard v-for="(todo, id) in todoCards"
-                :todo="todo"
-                :key="id"
-                class="list"
-                @removeTodo="removeTodo"
-                @toggleEditModal="toggleEditModal"
-      />
+    <TodoEdit v-model:is-open-modal="isOpenModal" />
+    <TodoForm :error-message="errorMessage" v-model:title="title" v-model:description="description"
+      @create-todo="createTodo" />
+    <div v-if="isLoading">
+      <p>Идет загрузка данных</p>
     </div>
-    <p v-else>
-      Список пуст
-    </p>
+
+    <div class="wrapper" v-else-if="todoCards?.length && !isLoading">
+      <TodoCard v-for="(todo, id) in todoCards" :todo="todo" :key="id" class="list" @removeTodo="removeTodo"
+        @toggleEditModal="toggleEditModal" />
+    </div>
+    <template v-else>
+      <p>
+        Список пуст
+      </p>
+      <p style="color: red;" v-if="errorResponse">
+        {{ errorResponse }}
+      </p>
+    </template>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import { onMounted, ref } from "vue";
 import TodoCard from "@/components/todo-card.vue";
 import TodoForm from "@/components/todo-form.vue";
-import {todoCards} from "@/const/mock-data.js";
 import TodoEdit from "@/components/todo-edit.vue";
+import axios from "axios";
 
-
+const todoCards = ref([])
 const title = ref("")
 const description = ref("")
-
+const isLoading = ref("")
 const errorMessage = ref("")
+const errorResponse = ref("")
 
 const createTodo = () => {
   if (!title.value.length || !description.value.length) {
@@ -58,7 +59,24 @@ const isOpenModal = ref(false);
 const toggleEditModal = () => {
   isOpenModal.value = !isOpenModal.value;
 }
+async function getTodos() {
+  try {
+    isLoading.value = true
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {})
+    todoCards.value = response.data
+    isLoading.value = false
+  } catch (err) {
+    errorResponse.value = "Ошибка при загрузке данных"
+    console.error(err)    
+  } finally {
+    isLoading.value = false
+  }
+}
 
+onMounted(() => {
+  console.log("Компонент монтирован")
+  getTodos()
+})
 </script>
 
 <style scoped>
